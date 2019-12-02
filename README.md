0c9527c82c052b9c025386bce82099f6fd99581c
yuuki const <806393858@qq.com>
2019-12-02 20:57:58 +0800

    添加readme，更新cloud版本

---
+++ README.md
@@ -0,0 +1,12 @@
+### COOKY CLOUD
+基于nacos的spring cloud项目- -<br>
+代码99.9999999999999999%摘抄 [FEBS CLOUD](https://github.com/wuyouzhuguli/FEBS-Cloud) 感谢帅逼鸟哥的贡献！
+#### 功能清单（待定）
+- [x] 权限管理
+- [X] Spring boot Admin
+- [X] skywalking
+- [X] 音乐搜索接口
+- [ ] 整合sentinel
+- [ ] 本地音乐（fastdfs）
+- [ ] 参数校验（前后端）
+- [ ] swagger
--- cooky-gateway/src/main/java/com/yuuki/cooky/gateway/handler/GatewayExceptionHandler.java
+++ cooky-gateway/src/main/java/com/yuuki/cooky/gateway/handler/GatewayExceptionHandler.java
@@ -60,7 +60,7 @@ public class GatewayExceptionHandler extends DefaultErrorWebExceptionHandler {
     }
 
     @Override
-    protected HttpStatus getHttpStatus(Map<String, Object> errorAttributes) {
-        return HttpStatus.INTERNAL_SERVER_ERROR;
+    protected int getHttpStatus(Map<String, Object> errorAttributes) {
+        return HttpStatus.INTERNAL_SERVER_ERROR.value();
     }
 }
